602404adcc932762814c0c8b6c7f2d415e967f9f
Leu Family <romanianfobjr@gmail.com>
2021-09-06 17:12:37 -0500

    get account by id implemented with middleware

--- api/accounts/accounts-middleware.js
+++ api/accounts/accounts-middleware.js
@@ -1,3 +1,5 @@
+const Account = require('./accounts-model')
+
 exports.checkAccountPayload = (req, res, next) => {
   // DO YOUR MAGIC
   console.log('check payload middleware')
@@ -10,8 +12,17 @@ exports.checkAccountNameUnique = (req, res, next) => {
   next()
 }
 
-exports.checkAccountId = (req, res, next) => {
+exports.checkAccountId = async (req, res, next) => {
   // DO YOUR MAGIC
-  console.log('check id middleware')
-  next()
+  try {
+    const account = await Account.getById(req.params.id)
+    if (!account) {
+      next({ status: 404, message: 'account not found' })
+    } else {
+      req.account = account
+      next()
+    }
+  } catch (err) {
+    next(err)
+  }
 }
