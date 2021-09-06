2acf5ca3cc2530f87c84d9dea0fbb9d3bd4ec5c8
Leu Family <romanianfobjr@gmail.com>
2021-09-06 17:35:53 -0500

    post endpoint functioning

--- api/accounts/accounts-model.js
+++ api/accounts/accounts-model.js
@@ -12,8 +12,11 @@ const getById = id => {
   return db('accounts').where('id', id).first()
 }
 
-const create = account => {
+const create = async account => {
   // DO YOUR MAGIC
+  // insert into accounts (name, budget) values ('foo', 1000);
+  const [id] = await db('accounts').insert(account)
+  return getById(id)
 }
 
 const updateById = (id, account) => {
--- api/accounts/accounts-router.js
+++ api/accounts/accounts-router.js
@@ -20,10 +20,11 @@ router.get('/:id', md.checkAccountId, async (req, res, next) => {
 router.post('/', 
  md.checkAccountPayload,
  md.checkAccountNameUnique,
- (req, res, next) => {
+ async (req, res, next) => {
   // DO YOUR MAGIC
   try {
-    res.json('post account')
+    const newAccount = await Account.create(req.body)
+    res.status(201).json(newAccount)
   } catch (err) {
     next(err)
   }
