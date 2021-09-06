6254f6fc61503469ef218eb4eda63550af76a3db
Leu Family <romanianfobjr@gmail.com>
2021-09-06 17:59:03 -0500

    fixed bug

--- api/accounts/accounts-router.js
+++ api/accounts/accounts-router.js
@@ -43,10 +43,9 @@ router.put('/:id',
  md.checkAccountPayload,
  async (req, res, next) => {
   // DO YOUR MAGIC
-  const updated = await Account.updateById(req.params.id, req.body)
-  res.json(updated)
   try {
-    res.json('update account')
+    const updated = await Account.updateById(req.params.id, req.body)
+    res.json(updated)
   } catch (err) {
     next(err)
   }
